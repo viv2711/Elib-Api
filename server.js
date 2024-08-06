c25f88907e34f617a1b99d299506e3cc2e2467d3
viv2711 <vivek.singh2021@vitbhopal.ac.in>
2024-08-07 00:16:13 +0530

    add basic formatting

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
-import {config}from "./src/config/config.js";
-
+import { config } from "./src/config/config.js";
 
 const PORT = config.port || 3000;
 const startServer = async () => {
-    await connectDB();
-    app.listen(PORT, () => {
-        console.log(`Server running on ${PORT}`)
-    })
-}
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+};
 
 startServer();
