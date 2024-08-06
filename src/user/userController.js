bd43b7531cb879c998ea489d25985bc34cfade50
viv2711 <vivek.singh2021@vitbhopal.ac.in>
2024-08-06 18:27:37 +0530

    added user schema

--- src/user/userController.js
+++ src/user/userController.js
@@ -1,15 +1,21 @@
 /* eslint-disable no-unused-vars */
-
-import createHttpError from "http-errors";
 // Request Processing for userRouters - userController
+import createHttpError from "http-errors";
+import userModel from "./userModel.js";
 export const createUser = async (req, res, next) => {
-    const {name, email, password} = req.body;
-    // Validation
-    if(!name || !email || !password){
-        const error = createHttpError(400, "All fields are required");
-        return next(error);
-    }
-    return res.json({msg: "User Created Successfully"})
-};
+  const { name, email, password } = req.body;
+  // Validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
 
-;
+  //Database Call
+  // If user already exists with an email ID
+  const user = await userModel.findOne({ email: email });
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email");
+    return next(error);
+  }
+  return res.json({ msg: "User Created Successfully" });
+};
---
+++ src/user/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+
+// User Schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// users collection in mongodb
+export default mongoose.model('User', userSchema);
