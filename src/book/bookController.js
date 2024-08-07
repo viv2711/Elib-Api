960c91f1a56efbba988155f0445a43a367ec7afe
viv2711 <vivek.singh2021@vitbhopal.ac.in>
2024-08-07 22:18:54 +0530

    create book

--- src/book/bookController.js
+++ src/book/bookController.js
@@ -1,11 +1,13 @@
 import path, { dirname } from "path";
 import cloudinary from "../config/cloudinary.js";
 import url from "url";
+import fs from "fs";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel.js";
 /* eslint-disable no-unused-vars */
 const createBook = async (req, res, next) => {
   // console.log("files", req.body, req.files);
-
+  const { title, genre } = req.body;
   //getting __fileName __dirName
   const __fileName = url.fileURLToPath(import.meta.url);
   const __dirname = dirname(__fileName);
@@ -21,7 +23,6 @@ const createBook = async (req, res, next) => {
     "../../public/data/uploads",
     fileName
   );
-  res.json({ msg: "Book Created Successfully" });
 
   // Uploading to Cloudinary Cloud from local path where image resides
   try {
@@ -32,6 +33,24 @@ const createBook = async (req, res, next) => {
       format: "pdf",
     });
     console.log("UploadResult", uploadResult);
+
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "66b24b853a68f3a83795e515",
+      file: uploadResult.secure_url,
+    });
+
+    // Delete temporary files after upload
+    try {
+      await fs.promises.unlink(filePath);
+    } catch (err) {
+      console.log(err);
+      return next(
+        createHttpError(500, "Error in deleting the temporary files")
+      );
+    }
+    res.status(201).json({ id: newBook._id });
   } catch (err) {
     console.log(err);
     return next(createHttpError(500, "Error while uploading on Cloudinary"));
